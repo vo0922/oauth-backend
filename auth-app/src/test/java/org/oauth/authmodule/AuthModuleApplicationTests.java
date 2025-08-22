a9062dfdaea1a66449cb5acb6c6f34dd447ddda6
sinuk <sinuk@telcoware.com>
2025-08-22 18:37:30 +0900

    [main] chore: setup Spring Boot multi-module project with Gradle
    - auth-app, resource-app, common-module, jpa-module 멀티 모듈 초기 구성
    - local 환경 구성
    - gradle 세팅 구성

---
+++ auth-app/src/test/java/org/oauth/authmodule/AuthModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.oauth.authmodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ jpa-module/src/main/java/org/oauth/jpamodule/JpaModuleApplication.java
@@ -0,0 +1,13 @@
+package org.oauth.jpamodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaModuleApplication.class, args);
+    }
+
+}
